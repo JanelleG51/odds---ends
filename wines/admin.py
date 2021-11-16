1019aea8611d78466e481f760707915930b5eba0
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-16 18:24:50 +0000

    feat: add all data to fixtures and migrate

--- wines/admin.py
+++ wines/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Wine, Case, Category
 
 # Register your models here.
+admin.site.register(Wine)
+admin.site.register(Case)
+admin.site.register(Category)
--- wines/models.py
+++ wines/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Wine(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    country = models.CharField(max_length=254)
+    region = models.CharField(max_length=254)
+    winery = models.CharField(max_length=254)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    year = models.CharField(max_length=254)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Custom model for cases #
+
+
+class Case(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
