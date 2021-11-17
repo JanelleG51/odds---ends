fe0c025c25de9b4e48640420e43e374e9b4efc7d
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-17 10:28:34 +0000

    db: updated admin functions in django database

--- wines/admin.py
+++ wines/admin.py
@@ -2,6 +2,40 @@ from django.contrib import admin
 from .models import Wine, Case, Category
 
 # Register your models here.
-admin.site.register(Wine)
-admin.site.register(Case)
-admin.site.register(Category)
+
+
+class WineAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'country',
+        'region',
+        'winery',
+        'rating',
+        'price',
+        'year',
+        'image',
+
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class CaseAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'image',
+
+    )
+
+
+admin.site.register(Wine, WineAdmin)
+admin.site.register(Case, CaseAdmin)
+admin.site.register(Category, CategoryAdmin)
