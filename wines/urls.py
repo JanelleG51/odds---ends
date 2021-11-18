5dacf34128955a41ca44e8b82f36cb7a14c19815
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-18 14:47:13 +0000

    fix: source and fix incorrect path to display correct case_id

--- wines/urls.py
+++ wines/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_wines, name='wines'),
-    path('<wine_id>', views.wine_detail, name='wine_detail'),
+    path('wine/<wine_id>', views.wine_detail, name='wine_detail'),
     path('cases/', views.all_cases, name='cases'),
-    path('<case_id>', views.case_detail, name='case_detail'),
-
+    path('case/<case_id>', views.case_detail, name='case_detail'),
 ]
