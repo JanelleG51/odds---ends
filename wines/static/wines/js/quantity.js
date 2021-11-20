e7b4afdf34631004fff4dac48c91003b87786fcf
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-20 22:20:15 +0000

    feat: add update and remove links to bag items

--- wines/static/wines/js/quantity.js
+++ wines/static/wines/js/quantity.js
@@ -39,3 +39,23 @@ $('.decrement-qty').click(function(e) {
    var itemId = $(this).data('item_id');
    handleEnableDisable(itemId);
 });
+
+// Update quantity on click
+$('.update-link').click(function(e) {
+    var form = $(this).prev('.update-form');
+    form.submit();
+})
+
+// Remove item and reload on click
+$('.remove-item').click(function(e) {
+    var csrfToken = "{{ csrf_token }}";
+    var itemId = $(this).attr('id').split('remove_')[1];
+    var type = $(this).data('type');
+    var url = `/bag/remove/${itemId}`;
+    var data = {'csrfmiddlewaretoken': csrfToken, 'type': type};
+
+    $.post(url, data)
+     .done(function() {
+         location.reload();
+     });
+})
