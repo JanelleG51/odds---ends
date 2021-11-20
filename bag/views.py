8dc6627007be7a013740658735d3f94bb476acc9
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-20 22:38:22 +0000

    feat: add view and url to adjust contents in bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -36,3 +36,27 @@ def add_to_bag(request, case_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def adjust_bag(request, case_id):
+    """ Add the quantity of the specified case in the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    type = None
+    if 'case_type' in request.POST:
+        type = request.POST['case_type']
+    bag = request.session.get('bag', {})
+
+    if type:
+        if quantity > 0:
+            bag[case_id]['items_by_type'][type] = quantity
+        else:
+            del bag[case_id]['items_by_type'][type]
+
+    else:
+        if quantity > 0:
+            bag[case_id] += quantity
+        else:
+            bag.pop[case_id]
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
