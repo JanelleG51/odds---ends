c7dfe4cf2cfe1bc29c0c3b46a99ec83286b2989b
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-21 20:43:31 +0000

    feat: add functionality to generate order number and update total Migrate all models to carry out these functions

--- checkout/models.py
+++ checkout/models.py
@@ -26,14 +26,50 @@ class Order(models.Model):
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_order_number(self):
+
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
+            'lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * \
+                settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False,
-                              on_delete=models.CASCADE, related_name='lineitems')
+    order = models.ForeignKey(Order, null=False,
+                              blank=False, on_delete=models.CASCADE,
+                              related_name='lineitems')
     case = models.ForeignKey(
         Case, null=False, blank=False, on_delete=models.CASCADE)
     case_type = models.CharField(
         max_length=2, null=True, blank=True)  # Basic, Premium, Superior
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(
-        max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6,
+                                         decimal_places=2,
+                                         null=False, blank=False,
+                                         editable=False)
+
+    def save(self, *args, **kwargs):
+
+        self.lineitem_total = self.case.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.case.sku} on order {self.order.order_number}'
