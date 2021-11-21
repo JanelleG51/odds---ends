6eb4c6b50a5bb3c48b46062873dc5bc360f64dae
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-21 23:11:19 +0000

    app: add checkout.html, checkout.css  and crispy_forms

--- odds_and_ends/settings.py
+++ odds_and_ends/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'wines',
     'bag',
     'checkout',
+
+    #Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'odds_and_ends.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,6 +83,9 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
         },
     },
 ]
