9fa60a74bb837351248c7f01ea0a3e914b155003
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-21 15:42:26 +0000

    fix: allow users to remove items from bag Move Jquery code to remove items from js file directly to bottom of bag.html to allow connection to csrf token

--- wines/static/wines/js/quantity.js
+++ wines/static/wines/js/quantity.js
@@ -44,20 +44,5 @@ $('.decrement-qty').click(function(e) {
 $('.update-link').click(function(e) {
     var form = $(this).prev('.update-form');
     form.submit();
-})
+});
 
-// Remove item and reload on click
-$('.remove-item').click(function(e) {
-    var csrfToken = "{{ csrf_token }}";
-    var itemId = $(this).attr('id').split('remove_')[1];
-    var type = $(this).data('case_type');
-    var url = `/bag/remove/${itemId}/`;
-    var data = {
-        'csrfmiddlewaretoken': csrfToken, 'case_type': type
-    };
-
-    $.post(url, data)
-     .done(function() {
-         location.reload();
-     });
-})
