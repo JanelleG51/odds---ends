20498a3901a03cc17aee4f97a59f3c08a82c4f75
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-22 12:29:42 +0000

    keys: add stripe keys to settings.py and environment

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 # Create your views here.
 
@@ -11,6 +15,10 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('cases'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
