8d92ae5f30d582c2346cb17f29dffe236e40bc49
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-22 17:11:59 +0000

    test: add WH SECRET and test webhook connection - success

--- odds_and_ends/settings.py
+++ odds_and_ends/settings.py
@@ -176,6 +176,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
 # Default primary key field type
