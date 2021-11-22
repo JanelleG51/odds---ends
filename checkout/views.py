9472eba1f78a6d550db5105bbba54f483c862d3c
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-22 13:03:41 +0000

    test: add and test payment intent is being recieved - success

--- checkout/views.py
+++ checkout/views.py
@@ -11,6 +11,10 @@ import stripe
 
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -19,13 +23,25 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        message.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': ' ',
-        'client_secret': 'test client secret'
+        'stripe_public_key': 'stripe_public_key',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
