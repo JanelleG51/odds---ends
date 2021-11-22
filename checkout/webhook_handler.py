df963d94d46e48a4edbef6cd30b826b0d00881de
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-22 16:53:57 +0000

    stripe: add payment intent models to webhooks handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -9,5 +9,17 @@ class StripeWH_Handler:
     def handle_event(self, event):
 
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
