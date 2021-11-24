add3ea51e9e3baa222cf6f4e9ede5544cda36cf8
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-24 22:46:30 +0000

    key: add secret key to Heroku config vars

--- odds_and_ends/settings.py
+++ odds_and_ends/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pxfju1dsrwh-*c4ybnjpk_+p+h20@qkmyq1&3r&j(ja*k#0@em'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['odds-and-ends.herokuapp.com', 'localhost']
 
