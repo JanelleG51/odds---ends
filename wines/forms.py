987e0c2a2cdce2670fa90c97917be18911c6ecbf
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-24 20:48:46 +0000

    admin: add image widget and adjust editing image field

--- wines/forms.py
+++ wines/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Wine, Case, Category
 
 
@@ -7,6 +8,9 @@ class WineForm(forms.ModelForm):
     class Meta:
         model = Wine
         fields = '__all__'
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -23,6 +27,9 @@ class CaseForm(forms.ModelForm):
     class Meta:
         model = Case
         fields = '__all__'
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
