a841b1798ca58c4e77dc0e4a5ffe228819399c4e
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-11-29 17:42:52 +0000

    fix: update qty in JS

--- wines/static/wines/js/quantity.js
+++ wines/static/wines/js/quantity.js
@@ -35,9 +35,11 @@ $('.decrement-qty').click(function(e) {
    e.preventDefault();
    var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
    var currentValue = parseInt($(closestInput).val());
-   $(closestInput).val(currentValue - 1);
-   var itemId = $(this).data('item_id');
-   handleEnableDisable(itemId);
+   if (currentValue >= 1){
+    $(closestInput).val(currentValue -1);
+    var itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
+   }
 });
 
 // Update quantity on click
