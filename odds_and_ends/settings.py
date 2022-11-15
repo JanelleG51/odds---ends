2a6e36e7ecb4eff8cce119cd42713c8af6d6025e
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2022-11-15 15:59:07 +0000

    update indentation error in settings.py

--- odds_and_ends/settings.py
+++ odds_and_ends/settings.py
@@ -32,8 +32,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['odds-and-ends.herokuapp.com', 'localhost']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
